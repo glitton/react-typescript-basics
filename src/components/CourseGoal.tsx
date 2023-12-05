6da24c175f2b316b6108971e7c1a2b60a9118185
glitton <glcodeworks@gmail.com>
2023-12-04 16:16:04 -0800

    components folder

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,11 @@
+export default function CourseGoal(props: string) {
+  return (
+    <article>
+      <div>
+        <h2>TITLE</h2>
+        <p>DESCRIPTION</p>
+        <button>Delete</button>
+      </div>
+    </article>
+  );
+}
